844df7dce2c814b7436438c08e88e39c337a6434
Vladimir <vladimirmyshkovski@gmail.com>
2018-11-10 05:33:44 +0400

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+extern crate rand;
+
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    println!("Угадай число!");
+
+    loop {
+
+	    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+	    println!("Пожалуйста, введите предположение.");
+
+	    let mut guess = String::new();
+
+	    io::stdin().read_line(&mut guess)
+	    	.expect("Не удалось прочитать строку!");
+
+	    let guess: u32 = match guess.trim().parse() {
+	    	Ok(num) => num,
+	    	Err(_) => continue,
+	    };
+
+	    println!("Ваша попытка: {}", guess);
+
+	    match guess.cmp(&secret_number) {
+	        Ordering::Less    => println!("Слишком маленькое!"),
+	        Ordering::Greater => println!("Слишком большое!"),
+	        Ordering::Equal   => {
+	        	println!("Вы выиграли!");
+	        	break;
+	        }
+	    }
+	}
+}
